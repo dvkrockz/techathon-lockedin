4621dc079cc7d8bebda4d8a9b488aa17b14f4aab
Mohit Bansal <Mohit.Bansal@gep.com>
2020-11-01 17:26:01 +0530

    Update DashBoardController.java

--- src/main/java/com/techathon/lockedin/dashboard/DashBoardController.java
+++ src/main/java/com/techathon/lockedin/dashboard/DashBoardController.java
@@ -40,70 +40,70 @@ public class DashBoardController {
 	}
 	
 	  @GetMapping("Authenticate")
-	    public JSONObject getAuthenticateData(String userName){
+	    public List<UserDetails> getAuthenticateData(String userName){
 
 	 
 
-	          JSONObject obj=new JSONObject();  
-	        if(userName == "mohit") {
-	            
-	            UserDetails detail1 = new UserDetails();
-	            detail1.setGitHubUserName("mohit-gep");
-	            detail1.setId((long) 1);
-	            detail1.setIsAdmin(true);
-	            detail1.setTotalReviewerPoints(200);
-
-	 
-
-	            UserDetails detail2 = new UserDetails();
-	            detail2.setGitHubUserName("mohit-gep");
-	            detail2.setId((long) 2);
-	            detail2.setIsAdmin(false);
-	            detail2.setTotalReviewerPoints(100);
-
-	 
-
-	             UserDetails detail3 = new UserDetails();
-	            detail3.setGitHubUserName("mohit-gep");
-	            detail3.setId((long) 3);
-	            detail3.setIsAdmin(false);
-	            detail3.setTotalReviewerPoints(150);
-
-	 
-
-	            UserDetails detail4 = new UserDetails();
-	            detail4.setGitHubUserName("mohit-gep");
-	            detail4.setId((long) 4);
-	            detail4.setIsAdmin(false);
-	            detail4.setTotalReviewerPoints(20);
-	            
-	            List<UserDetails> details = null;
-	            details.add(detail1);
-	            details.add(detail2);
-	            details.add(detail3);
-	            details.add(detail4);
-	               
-	          obj.put("isAdmin","true");    
-	          obj.put("data",details);
-	        }
-	        else {
-	             UserDetails detail1 = new UserDetails();
-	                detail1.setGitHubUserName("mohit-gep");
-	                detail1.setId((long) 1);
-	                detail1.setIsAdmin(false);
-	                detail1.setTotalReviewerPoints(200);
-
-	 
-
-	                
-	                List<UserDetails> details = null;
-	                details.add(detail1);
-	                
-	                  
-	              obj.put("isAdmin","false");    
-	              obj.put("data",details);
-	        }
-	        return obj;
+//	          JSONObject obj=new JSONObject();  
+//	        if(userName == "mohit") {
+//	            
+//	            UserDetails detail1 = new UserDetails();
+//	            detail1.setGitHubUserName("mohit-gep");
+//	            detail1.setId((long) 1);
+//	            detail1.setIsAdmin(true);
+//	            detail1.setTotalReviewerPoints(200);
+//
+//	 
+//
+//	            UserDetails detail2 = new UserDetails();
+//	            detail2.setGitHubUserName("mohit-gep");
+//	            detail2.setId((long) 2);
+//	            detail2.setIsAdmin(false);
+//	            detail2.setTotalReviewerPoints(100);
+//
+//	 
+//
+//	             UserDetails detail3 = new UserDetails();
+//	            detail3.setGitHubUserName("mohit-gep");
+//	            detail3.setId((long) 3);
+//	            detail3.setIsAdmin(false);
+//	            detail3.setTotalReviewerPoints(150);
+//
+//	 
+//
+//	            UserDetails detail4 = new UserDetails();
+//	            detail4.setGitHubUserName("mohit-gep");
+//	            detail4.setId((long) 4);
+//	            detail4.setIsAdmin(false);
+//	            detail4.setTotalReviewerPoints(20);
+//	            
+//	            List<UserDetails> details = null;
+//	            details.add(detail1);
+//	            details.add(detail2);
+//	            details.add(detail3);
+//	            details.add(detail4);
+//	               
+//	          obj.put("isAdmin","true");    
+//	          obj.put("data",details);
+//	        }
+//	        else {
+//	             UserDetails detail1 = new UserDetails();
+//	                detail1.setGitHubUserName("mohit-gep");
+//	                detail1.setId((long) 1);
+//	                detail1.setIsAdmin(false);
+//	                detail1.setTotalReviewerPoints(200);
+//
+//	 
+//
+//	                
+//	                List<UserDetails> details = null;
+//	                details.add(detail1);
+//	                
+//	                  
+//	              obj.put("isAdmin","false");    
+//	              obj.put("data",details);
+//	        }
+	        return userRepo.findAll();
 	    }
 	    
 	    
