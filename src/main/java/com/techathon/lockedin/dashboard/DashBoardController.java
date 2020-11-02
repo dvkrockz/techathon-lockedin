b41719580a7b46eb6c859fd41dd95c460c61d71d
Mohit Bansal <Mohit.Bansal@gep.com>
2020-11-02 20:12:09 +0530

    Update DashBoardController.java

--- src/main/java/com/techathon/lockedin/dashboard/DashBoardController.java
+++ src/main/java/com/techathon/lockedin/dashboard/DashBoardController.java
@@ -40,7 +40,7 @@ public class DashBoardController {
 //	
 	 
 //	@Scheduled(cron="0 0 0 1 */3 *")
-	@Scheduled(cron="0 0/1 * * * ?")
+//	@Scheduled(cron="0 0/1 * * * ?")
 	public void schedularJob() throws ParseException, IOException {
 	    // Something
  
